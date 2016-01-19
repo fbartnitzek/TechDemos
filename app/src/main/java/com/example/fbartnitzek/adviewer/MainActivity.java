d7613ca0b8bc80acff7e5c8c3ec332efc0f4e8a1
fbartnitzek <frank_bartnitzek@web.de>
2016-01-19 23:26:20 +0100

    added simple banner ad activity and some buttons

--- app/src/main/java/com/example/fbartnitzek/adviewer/MainActivity.java
+++ app/src/main/java/com/example/fbartnitzek/adviewer/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.fbartnitzek.adviewer;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +17,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    public void launchBannerAd(View view) {
+        startActivity(new Intent(this, BannerActivity.class));
+    }
+
+    public void launchInterstitialAd(View view) {
+//        startActivity(new Intent(this, BannerActivity.class));
     }
 
     @Override
